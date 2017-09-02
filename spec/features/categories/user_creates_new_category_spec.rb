e5fbaac5de242b81cd519ba105ac3b879716f36d
mimilettd <mimilettd@gmail.com>
2017-09-01 21:46:32 -0600

    User can create category through job form

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "User creates new category" do
-  scenario "a user can create a new category" do
+  scenario "a user can create a new category from category index" do
 
     category = "Business Analyst"
 
@@ -23,5 +23,19 @@ RSpec.feature "User creates new category" do
     click_button "Create Category"
 
     expect(current_path).to eq(new_category_path)
+  end
+
+  scenario "a user creates a new category when creating a job" do
+    company = create(:company)
+    create_list(:category, 5)
+
+    visit new_company_job_path(company)
+    click_link "Create a New Category"
+
+    fill_in "Title", with: "Front-End"
+    click_button "Create Category"
+
+    expect(current_path).to eq(category_path(Category.last))
+    expect(page).to have_content("Front-End")
   end
 end
