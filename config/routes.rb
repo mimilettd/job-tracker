009f2f21f12199fcc859821a44a5f9e19cda7f09
mimilettd <mimilettd@gmail.com>
2017-09-03 11:35:00 -0600

    set root to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,9 @@ Rails.application.routes.draw do
     resources :contacts
   end
 
-  resources :dashboard
+  resources :dashboard, only: [:index]
 
+  root to: 'dashboard#index'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- public/vendor/bootstrap/css/application.js
+++
@@ -1,6 +0,0 @@
-//= require rails-ujs
-//= require turbolinks
-//= require Chart.bundle
-//= require chartkick
-//= require_tree
-//= require highcharts
