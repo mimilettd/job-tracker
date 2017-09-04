314a37e48984a7ac9e9baf1b78442b360efb2494
mimilettd <mimilettd@gmail.com>
2017-09-04 09:22:14 -0600

    implement Level of Interest by Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,9 @@ class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :contacts
+
+  def self.print_name(id)
+    print_name = find(id)
+    print_name.name
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -24,6 +24,4 @@ class Job < ApplicationRecord
     .count
   end
 
-
-
 end
